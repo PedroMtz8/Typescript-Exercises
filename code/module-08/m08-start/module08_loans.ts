d1f987f9ae4fc24c414d86ad1aaf6dce696f2df7
Pedro Martinez <pedrocmartinez568@gmail.com>
2022-11-11 10:32:33 -0600

    finish

---
+++ code/module-08/m08-start/module08_loans.ts
@@ -0,0 +1,12 @@
+
+
+/*  TODO Create LoanPrograms namespace. */
+namespace Loans {
+    export interface Loan{
+        principle: number,
+        interestRate: number
+    }
+    export interface ConventionalLoan extends Loan{
+        months: number
+    }
+}
